30b6b3204b8d713399f58d0dc9340e7cd5bd5624
Topher <33035529+robinsonchtopher@users.noreply.github.com>
2022-09-28 21:20:06 -0400

    Update script.py

--- script.py
+++ script.py
@@ -146,10 +146,11 @@ class Robit:
     def findgame(self):
         gamesplayed=0
         for i in range(0,15):
+            print(pyautogui.pixel(1670,1041)[1])
             if pyautogui.pixel(1670,1041)[1] >= 150:#check if the green value is over 150 the actual value is about 180 
                 self.inGame=False
             else:
-                if gamesplayed == 15:
+                if gamesplayed < 15:
                     self.down()
                     gamesplayed+=1
                 else:
